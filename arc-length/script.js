741507933e80bb74cd1914d0ac8dd6646a05106a
displayflex <3tonishere@gmail.com>
2018-10-13 16:31:59 +0300

    fix

--- arc-length/script.js
+++ arc-length/script.js
@@ -1,24 +1,43 @@
-let width = document.querySelector('.width');
-let height = document.querySelector('.height');
-let calculateButton = document.querySelector('.calculate-button');
-let resultArea = document.querySelector('.result');
+let arcLengthWidth = document.querySelector('.arc-length__width');
+let arcLengthHeight = document.querySelector('.arc-length__height');
+let arcLengthCalculateBtn = document.querySelector('.arc-length__calculate-btn');
+let arcLengthResult = document.querySelector('.arc-length__result');
 
-calculateButton.addEventListener('click', function(e) {
+let circleRadiusLength = document.querySelector('.circle-radius__length');
+let circleRadiusCalculateBtn = document.querySelector('.circle-radius__calculate-btn');
+let circleRadiusResult = document.querySelector('.circle-radius__result');
+
+arcLengthCalculateBtn.addEventListener('click', function(e) {
 	e.preventDefault();
-	resultArea.innerHTML = "";
+	arcLengthResult.innerHTML = "";
 
-	let widthValue = +width.value;
-	let heightValue = +height.value;
+	let arcLengthWidthValue = +arcLengthWidth.value;
+	let arcLengthHeightValue = +arcLengthHeight.value;
 
-	let chord = Math.sqrt(Math.pow(heightValue, 2) + Math.pow(widthValue/2, 2));
+	let chord = Math.sqrt(Math.pow(arcLengthHeightValue, 2) + Math.pow(arcLengthWidthValue/2, 2));
 
-	let arcLength = (2 * chord) + (1/3 * (2 * chord - widthValue));
+	let arcLength = (2 * chord) + (1/3 * (2 * chord - arcLengthWidthValue));
 	arcLength = Math.round(arcLength * 10) / 10;
 
-	// let radius = (heightValue / 2) + (Math.pow(widthValue, 2) / (8 * heightValue));
+	// let radius = (arcLengthHeightValue / 2) + (Math.pow(arcLengthWidthValue, 2) / (8 * arcLengthHeightValue));
 
-	resultArea.innerHTML += "Ширина = " + widthValue + " см <br>";
-	resultArea.innerHTML += "Высота = " + heightValue + " см <br><br>";
-	resultArea.innerHTML += "<b>Длина дуги = " + arcLength + " см <b><br>";
-	resultArea.innerHTML += "<small>(по формуле Гюйгенса)</small><br>";
+	arcLengthResult.innerHTML += "Ширина = " + arcLengthWidthValue + " см <br>";
+	arcLengthResult.innerHTML += "Высота = " + arcLengthHeightValue + " см <br>";
+	arcLengthResult.innerHTML += "<b>Длина дуги = " + arcLength + " см <b><br>";
+	arcLengthResult.innerHTML += "<small>(по формуле Гюйгенса)</small><br>";
 });
+
+circleRadiusCalculateBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	circleRadiusResult.innerHTML = "";
+
+	let circleRadiusLengthValue = +circleRadiusLength.value;
+
+	let diameter = (circleRadiusLengthValue / (2 * Math.PI)) * 2;
+	diameter = Math.round(diameter * 10) / 10;
+
+	circleRadiusResult.innerHTML += "Длина окружности = " + circleRadiusLengthValue + " см <br>";
+	circleRadiusResult.innerHTML += "<b>Диаметр окружности = " + diameter + " см <b><br>";
+});
+
+
